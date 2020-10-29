028091b618c3d6c8df97cf7d469f800ba4aa542b
Francisco Javier Delgado Romero <37573041+FranJDR@users.noreply.github.com>
2020-10-29 11:52:00 +0100

    a

--- src/app/shared/services/participants/participants.service.ts
+++ src/app/shared/services/participants/participants.service.ts
@@ -14,7 +14,27 @@ export class ParticipantsService {
   singUpParcipant(participant: Participant): Promise<any> {
     return this.httpClient.post(this.url, participant).toPromise()
       .then(() => {
-        console.log('Usuario creado');
+        console.log('Usuario creado.');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  editParcipant(user: Participant): Promise<any> {
+    return this.httpClient.put(this.url + '/' + user.id, user).toPromise()
+      .then(() => {
+        console.log('Usuario editado.');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  deleteParcipant(id: string): Promise<any> {
+    return this.httpClient.delete(this.url + '/' + id).toPromise()
+      .then(() => {
+        console.log('Usuario eliminado.');
       })
       .catch(err => {
         console.log(err);
